32b93b90af62d9b01588995fcde9970bb5e5ea4c
Anton <antderar1488_antnoman2517@mail.ru>
2023-11-25 17:38:09 +0300

    Очередная версия

--- app/src/main/java/com/practicum/playlistmaker/MediaLibraryActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MediaLibraryActivity.kt
@@ -1,11 +1,11 @@
-package com.example.playlistmaker
+package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MediaLibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        setContentView(R.layout.activity_media_library)
     }
 }
