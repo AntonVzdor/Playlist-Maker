db13926e0c26397f21bfb443a31a004d683242b1
dernom <der_nom@mail.ru>
2023-11-30 13:17:20 +0300

    загрузка из архива

--- app/src/main/java/com/example/playlistmaker/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MediaLibraryActivity.kt
@@ -1,8 +1,9 @@
-package com.practicum.playlistmaker
+package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.example.playlistmaker.R
+//check this
 class MediaLibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
