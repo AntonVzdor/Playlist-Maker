c8194ddbd54692ab2892b7612628636d313c3668
dernom <der_nom@mail.ru>
2023-12-25 16:28:06 +0300

    12345

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,10 +1,8 @@
 package com.practicum.playlistmaker
 
-import com.google.gson.annotations.SerializedName
-
 data class Track(
     val trackName: String,
     val artistName: String,
-    @SerializedName("trackTimeMillis") val trackTime: String,
+    val trackTime: String,
     val artworkUrl100: String
 )
--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -1,8 +1,5 @@
 package com.practicum.playlistmaker
 
-import android.content.Context
-import android.icu.text.SimpleDateFormat
-import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,16 +8,34 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.example.playlistmaker.R
-import java.util.Locale
-
 
 class TrackAdapter(private val tracks: List<Track>) :
     RecyclerView.Adapter<TrackAdapter.TrackViewHolder>() {
 
 
+    class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val tvTrackName: TextView = itemView.findViewById(R.id.tv_trackName)
+        private val tvArtistName: TextView = itemView.findViewById(R.id.tv_artistName)
+        private val tvTrackTime: TextView = itemView.findViewById(R.id.tv_trackTime)
+        private val ivArtwork: ImageView = itemView.findViewById(R.id.iv_artwork)
+
+        fun bind(model: Track) {
+            tvTrackName.text = model.trackName
+            tvArtistName.text = model.artistName
+            tvTrackTime.text = model.trackTime
+            Glide.with(this.itemView.context)
+                .load(model.artworkUrl100)
+                .fitCenter()
+                .dontAnimate()
+                .placeholder(R.drawable.placeholder)
+                .transform(RoundedCorners(2))
+                .into(ivArtwork)
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_track, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.activity_item_track, parent, false)
         return TrackViewHolder(view)
     }
 
@@ -32,29 +47,5 @@ class TrackAdapter(private val tracks: List<Track>) :
         holder.bind(tracks[position])
     }
 
-    class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val tvTrackName: TextView = itemView.findViewById(R.id.tv_trackName)
-        private val tvArtistName: TextView = itemView.findViewById(R.id.tv_artistName)
-        private val tvTrackTime: TextView = itemView.findViewById(R.id.tv_trackTime)
-        private val ivArtwork: ImageView = itemView.findViewById(R.id.iv_artwork)
-
-        fun bind(model: Track) {
-            tvTrackName.text = model.trackName
-            tvArtistName.text = model.artistName
-            tvTrackTime.text =
-                SimpleDateFormat("mm:ss", Locale.getDefault()).format(model.trackTime.toLong())
-            Glide.with(this.itemView.context)
-                .load(model.artworkUrl100)
-                .fitCenter().dontAnimate()
-                .placeholder(R.drawable.placeholder)
-                .transform(RoundedCorners(dpToPx(8F, itemView.context))).into(ivArtwork)
-        }
-
-        fun dpToPx(dp: Float, context: Context): Int {
-            return TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics
-            ).toInt()
-        }
-    }
 
 }
